bebfbf62d909d4dfd90e89922759b761d5a080ca
Seizure <78146652+ConvulsiveSeizure@users.noreply.github.com>
2021-02-13 17:34:07 +0300

    dev TCPClient and TCPServer

---
+++ TCPClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClientApp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            const string IP = "127.0.0.1";
+            const int PORT = 8888;
+
+            Console.WriteLine("Etner username: ");
+            string userName = Console.ReadLine();
+
+            TcpClient client = null;
+            try 
+            {
+
+                client = new TcpClient(IP, PORT);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    Console.WriteLine($"{userName}: ");
+                    string message = Console.ReadLine();
+                    message = String.Format("{0}: {1}", userName, message);
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+
+                    } while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine($"Server: {message}");
+
+                }
+                
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TcpListenerApp
+{
+    public class ClientObject {
+        public TcpClient client;
+        public ClientObject(TcpClient tcpClient) {
+            client = tcpClient;
+        }
+        public void Procces()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[256];
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                    message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
+                    data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    class Program
+    {
+        const int port = 8888;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Procces));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            
+        }
+    }
+}
